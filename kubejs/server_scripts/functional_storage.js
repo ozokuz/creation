990b94fc8cfa08ab8410b43b869481a621645177
Ozoku <19756856+ozokuz@users.noreply.github.com>
2023-07-27 15:24:44 +0300

    mod updates & remove broken drawer recipes

---
+++ kubejs/server_scripts/functional_storage.js
@@ -0,0 +1,5 @@
+ServerEvents.recipes((event) => {
+  event.remove({ id: "functionalstorage:oak_drawer_alternate_x1" });
+  event.remove({ id: "functionalstorage:oak_drawer_alternate_x2" });
+  event.remove({ id: "functionalstorage:oak_drawer_alternate_x4" });
+});
