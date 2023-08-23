70893a9fe8cad7b5ce313bafc84994e265c86ef0
Ozoku <19756856+ozokuz@users.noreply.github.com>
2023-08-23 18:09:29 +0300

    1.8.1 - add some custom polished stone blocks

---
+++ kubejs/server_scripts/custom.js
@@ -0,0 +1,18 @@
+ServerEvents.recipes((event) => {
+  event.recipes.minecraft.stonecutting(
+    "kubejs:polished_stone",
+    "minecraft:stone"
+  );
+  event.recipes.minecraft.stonecutting(
+    "kubejs:dark_polished_stone",
+    "minecraft:stone"
+  );
+  event.recipes.minecraft.stonecutting(
+    "kubejs:darker_polished_stone",
+    "minecraft:stone"
+  );
+  event.recipes.minecraft.stonecutting(
+    "kubejs:darkest_polished_stone",
+    "minecraft:stone"
+  );
+});
---
+++ kubejs/startup_scripts/custom.js
@@ -0,0 +1,6 @@
+StartupEvents.registry("block", (event) => {
+  event.create("polished_stone");
+  event.create("dark_polished_stone");
+  event.create("darker_polished_stone");
+  event.create("darkest_polished_stone");
+});
